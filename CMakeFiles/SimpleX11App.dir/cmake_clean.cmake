e3324ed34660c29fa9978f6d90244341db5b0e84
lukasz <lukaszcholewka@outlook.com>
2025-06-15 13:26:41 +0200

    Init

---
+++ CMakeFiles/SimpleX11App.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleX11App.dir/src/main.cpp.o"
+  "CMakeFiles/SimpleX11App.dir/src/main.cpp.o.d"
+  "SimpleX11App"
+  "SimpleX11App.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleX11App.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
